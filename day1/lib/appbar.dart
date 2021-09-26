e12f917197580a8070493986a17e3f3221f5d7f1
Tirtho 304 <shirdartho15-13304@diu.edu.bd>
2021-09-26 10:53:14 +0600

    Day2 complited

---
+++ day1/lib/appbar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+class appbar extends StatefulWidget {
+  
+  @override
+  _appbarState createState() => _appbarState();
+}
+
+class _appbarState extends State<appbar> {
+  int day = 30;
+  String name = "Tirtho";
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          
+          title: Text("HomePage",
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+        ),
+        body: Center(
+          child: Container(
+            child: Text("Welcome ${name} ${day} of flutter"),
+          )
+        ),
+        drawer: Drawer(),
+      ),
+      
+    );
+  }
+}
--- day1/lib/main.dart
+++ day1/lib/main.dart
@@ -1,3 +1,5 @@
+//import 'package:day1/appbar.dart';
+import 'package:day1/AppBar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,8 +16,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    
+    return MaterialApp(
+      home:appbar(),
     );
   }
 }
